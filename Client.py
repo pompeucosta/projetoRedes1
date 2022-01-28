8336decba718c8283a9fe1fc92be735c3b9cff6d
Pompeu Costa <pompeugcosta@gmail.com>
2022-01-28 17:42:42 +0000

    Codigo python alterado; optimizacoes feitas no envio das mensagens (menos bytes) e server mostra memoria com base em GB em vez de so bytes

--- Client.py
+++ Client.py
@@ -1,10 +1,11 @@
 import socket
 import signal
+import struct
 import sys
-import threading
 import psutil
 import time
 import os
+import math
 
 
 def signal_handler(sig, frame):
@@ -22,19 +23,25 @@ tcp_port = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.connect((ip_addr, tcp_port))
+msg = sock.recv(4096)
+ID = struct.unpack('!I',msg)[0]
+
+print("Conected with ID: {}".format(ID))
 
         
 
 while True:
     try:
         process = psutil.Process(os.getpid())
-        cpuusage = str(psutil.cpu_percent(interval=1))
-        memoryusage = str(process.memory_info().rss)
-        message = "CPU "+ cpuusage + " RAM " + memoryusage + " bytes"
-        sock.send(message.encode())
+        cpuusage = int(psutil.cpu_percent(interval=1))
+        memoryusage = psutil.virtual_memory().used
+        exp = int(len(str(memoryusage)) / 3)
+        memoryusage = (float)(memoryusage / math.pow(1024,exp))
+        message = struct.pack('!IIf',cpuusage,exp,round(memoryusage,2))
+        sock.send(message)
         response = sock.recv(4096).decode()
         print('Server response: {}'.format(response))
-        time.sleep(1)
+        time.sleep(5)
             
     except (socket.timeout, socket.error):
         print('Server error. Done!')
--- Server.py
+++ Server.py
@@ -1,7 +1,13 @@
+import math
 import socket
+import struct
 import threading
 import signal
 import sys
+
+METRIC = ["bytes","KB","MB","GB"]
+
+user_id = 0
 
 def signal_handler(sig, frame):
     print('\nDone!')
@@ -11,20 +17,23 @@ signal.signal(signal.SIGINT, signal_handler)
 print('Press Ctrl+C to exit...')
 
 
-def handle_client_connection(client_socket,address): 
-    print('Accepted connection from {}:{}'.format(address[0], address[1]))
+def handle_client_connection(client_socket,address,user_id,metric): 
+    print('Accepted connection from {}:{} \nID:{}'.format(address[0], address[1],user_id))
     try:
         while True:
             request = client_socket.recv(1024)
             if not request:
                 client_socket.close()
             else:
-                msg=request.decode()
-                print('Received {}'.format(msg))
-                msg=("ECHO: "+msg).encode()
+                msg=struct.unpack('!IIf',request)
+                cpu_usage = msg[0]
+                exp = msg[1]
+                memory_usage = msg[2]
+                print('Received from {}: cpu: {}% ; memory: {} {}'.format(user_id,cpu_usage,round(memory_usage,2),metric[exp]))
+                msg=("Received").encode()
                 client_socket.send(msg)
     except (socket.timeout, socket.error):
-        print('Client {} error. Done!'.format(address))
+        print('Client {} error. Done!'.format(user_id))
 
 ip_addr = "0.0.0.0"
 tcp_port = 5005
@@ -37,6 +46,9 @@ print('Listening on {}:{}'.format(ip_addr, tcp_port))
 
 while True:
     client_sock, address = server.accept()
-    client_handler = threading.Thread(target=handle_client_connection,args=(client_sock,address),daemon=True)
+    msg = struct.pack('!I',user_id)
+    client_sock.send(msg)
+    client_handler = threading.Thread(target=handle_client_connection,args=(client_sock,address,user_id,METRIC),daemon=True)
     client_handler.start()
+    user_id += 1
 
